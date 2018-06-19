6ca0eb7cd71e6658a0dc672e0569355188564393
pasawaya <psawaya@collegiateschool.org>
2018-06-19 11:55:19 -0400

    Added deletion of videos with no annotations

--- consolidator.py
+++ consolidator.py
@@ -20,6 +20,7 @@ if not os.path.exists(videos_path):
 if not os.path.exists(annotations_path):
     os.makedirs(annotations_path)
 
+# Move all batch directories to main video and annotation directories
 for directory in os.listdir(root):
     directory = os.path.join(root, directory)
     if os.path.isdir(directory) and "videos_" in directory or "annotations_" in directory:
@@ -28,3 +29,27 @@ for directory in os.listdir(root):
                 destination = videos_path if 'videos_' in directory else annotations_path
                 if not os.path.exists(os.path.join(destination, subdirectory)):
                     shutil.move(os.path.join(directory, subdirectory), destination)
+
+
+for directory in os.listdir(annotations_path):
+    annotations_directory = os.path.join(annotations_path, directory)
+    if os.path.isdir(annotations_directory):
+        video_directory = os.path.join(videos_path, directory)
+
+        # Delete videos with no annotations
+        for root, subdir, files in os.walk(annotations_directory):
+
+            # Check if video has any annotations
+            has_annotations = '.png' in ' '.join(files)
+
+            # Delete directories without annotations
+            if not has_annotations:
+                shutil.rmtree(video_directory)
+                shutil.rmtree(annotations_directory)
+                break
+
+        # If video has annotations, resize and in-paint
+        if os.path.exists(video_directory):
+            print('remains')
+
+
