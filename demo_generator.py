7a7ef093421dab7a34c0f3c60b635501edcfa8d3
pasawaya <psawaya@collegiateschool.org>
2018-09-03 18:44:04 -0400

    Cleaned

--- demo_generator.py
+++ demo_generator.py
@@ -31,16 +31,12 @@ sf_path = os.path.join(args.data_dir, 'surface_normals')
 n_images = len(os.listdir(frames_path))
 image_names = [str(i) + '.png' for i in range(n_images)]
 
-fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-out = cv2.VideoWriter(args.output_name, fourcc, 8.0, (args.w * 4, args.h))
+out = cv2.VideoWriter(args.output_name, cv2.VideoWriter_fourcc(*'MJPG'), 8.0, (args.w * 4, args.h))
 
 for name in image_names:
-    frame = cv2.imread(os.path.join(frames_path, name))
-    inpainted = cv2.imread(os.path.join(inpainted_path, name))
-    mask = cv2.imread(os.path.join(masks_path, name))
-    sf = cv2.imread(os.path.join(sf_path, name))
-
-    combined = np.hstack([frame, inpainted, mask, sf])
-    out.write(combined)
+    out.write(np.hstack([cv2.imread(os.path.join(frames_path, name)),
+                         cv2.imread(os.path.join(inpainted_path, name)),
+                         cv2.imread(os.path.join(masks_path, name)),
+                         cv2.imread(os.path.join(sf_path, name))]))
 
 out.release()
