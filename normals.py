3c588f3a4627469b0c9f69fe280dfeca70481bb2
pasawaya <psawaya@collegiateschool.org>
2018-09-03 19:27:44 -0400

    Added bulk folder deleting, corrected surface normals code

--- normals.py
+++ normals.py
@@ -1,23 +1,25 @@
 
-import cv2
 import os
+import cv2
+from os_utils import safe_makedirs, del_dirs
 from scipy.misc import imsave
 from subprocess import call, DEVNULL
 
 
 def surface_normals(image):
-    image_path = 'temp_image.png'
-    normals_path = 'temp_normals.png'
+    temp_dir = 'normals_temp'
+    safe_makedirs(temp_dir)
+    image_path = os.path.join(os.getcwd(), temp_dir, 'temp_image.png')
+    normals_path = os.path.join(os.getcwd(), temp_dir, 'temp_normals.png')
 
     # Run Taskonomy command on temporarily saved image
     imsave(image_path, image)
     cmd = 'python taskonomy/taskbank/tools/run_img_task.py --task rgb2sfnorm ' \
           '--img ' + image_path + ' --store ' + normals_path
     call(cmd, shell=True, stdout=DEVNULL, stderr=DEVNULL)
-    os.remove(image_path)
 
-    # Load results from file and delete file
+    # Load results from file and clean-up temporary images and folders
     sf = cv2.imread(normals_path)
     sf = cv2.cvtColor(sf, cv2.COLOR_BGR2RGB)
-    os.remove(normals_path)
+    del_dirs(temp_dir)
     return sf
