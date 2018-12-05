fc0f9bd847efbd71a1e692864de97da09601c79e
pasawaya <psawaya@collegiateschool.org>
2018-12-05 16:25:28 -0500

    switched to opencv mask r-cnn preliminarily

--- segmentation/mask.py
+++ segmentation/mask.py
@@ -1,6 +1,8 @@
 from segmentation.mrcnn import model as modellib
 from segmentation.mrcnn import coco
 import numpy as np
+import cv2
+
 
 # Suppress AVX, etc. warnings when running on CPU
 import os
@@ -34,12 +36,26 @@ class MaskRCNN:
         self.classes = [class_names.index(name) for name in classes]
 
     def detect(self, image):
-        result = self.model.detect([image])[0]
-        classes = result['class_ids']
-        matches = [i for i, class_id in enumerate(classes) if class_id in self.classes]
+        textGraph = "./mask_rcnn_inception_v2_coco_2018_01_28.pbtxt"
+        modelWeights = "./mask_rcnn_inception_v2_coco_2018_01_28/frozen_inference_graph.pb"
 
-        scores = result['scores'][matches]
-        masks = result['masks'][:, :, matches]
-        masks = np.moveaxis(masks, 2, 0) * 255
-        return scores, masks.astype(np.uint8)
+        net = cv2.dnn.readNetFromTensorflow(modelWeights, textGraph)
+        net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
+        net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+        blob = cv2.dnn.blobFromImage(image, swapRB=True, crop=False)
+        net.setInput(blob)
+
+        boxes, masks = net.forward(['detection_out_final', 'detection_masks'])
+        print(boxes.shape)
+        print(masks.shape)
+        return 0
+        # result = self.model.detect([image])[0]
+        # classes = result['class_ids']
+        # matches = [i for i, class_id in enumerate(classes) if class_id in self.classes]
+        #
+        # scores = result['scores'][matches]
+        # masks = result['masks'][:, :, matches]
+        # masks = np.moveaxis(masks, 2, 0) * 255
+        # return scores, masks.astype(np.uint8)
 
