933f1a2a3bed4a438c5d72bc1d49916de81bcac0
pasawaya <psawaya@collegiateschool.org>
2018-12-06 19:13:28 -0500

    Debugging

--- normals.py
+++ normals.py
@@ -16,10 +16,13 @@ def surface_normals(image):
     imsave(image_path, image)
     cmd = 'python taskonomy/taskbank/tools/run_img_task.py --task rgb2sfnorm ' \
           '--img \"' + image_path + '\" --store \"' + normals_path + '\"'
-    call(cmd, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+
+    # call(cmd, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+    call(cmd, shell=True)
 
     # Load results from file and clean-up temporary images and folders
     sf = cv2.imread(normals_path)
+    print(sf.shape)
     sf = cv2.cvtColor(sf, cv2.COLOR_BGR2RGB)
     del_dirs(temp_dir)
     return sf
